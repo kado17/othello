0cae0b07a53a24d2915e258ba9b86f813392ac20
s1f102100119 <s1f102100119@iniad.org>
2022-07-07 02:41:52 +0900

    不要機能の削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import type { Socket } from 'socket.io-client'
 import { io } from 'socket.io-client'
 import styled from 'styled-components'
 
-const COLOR = ['white', 'black']
 const URL = 'http://localhost:8000'
+const COLOR = ['white', 'black']
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
@@ -31,13 +31,13 @@ const Sqaure = styled.div<{ num: number }>`
   height: 8vh;
   font-size: 12vh;
   line-height: 4.5vh;
-  color: ${({ num }) => (0 === num || num === 1 ? COLOR[num] : '')};
   text-align: center;
   vertical-align: bottom;
+  background-color: ${({ num }) => (num === 8 ? 'red' : '')};
   border: 0.2vh solid black;
 `
 
-const WhiteDisk = styled.div`
+const Disk = styled.div<{ num: number }>`
   position: absolute;
   top: 0;
   right: 0;
@@ -46,24 +46,11 @@ const WhiteDisk = styled.div`
   width: 5vh;
   height: 5vh;
   margin: auto;
-  background-color: white;
+  background-color: ${({ num }) => (num === 0 || num === 1 ? COLOR[num] : '')};
   border-radius: 50%;
 `
 
-const BlackDisk = styled(WhiteDisk)`
-  background-color: black;
-`
-
-const ColumnArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 75vh;
-  margin: 0 3vh;
-  font-size: 0;
-`
-
-const GameStateShow = styled.div`
+const GameStateLabel = styled.label`
   height: 8vh;
   margin: 0 0 1vh;
   font-size: 3vh;
@@ -73,36 +60,6 @@ const GameStateShow = styled.div`
   text-align: center;
 `
 
-const PlayerWhite = styled.div`
-  width: 26vh;
-  height: 8vh;
-  margin: 25vh 0;
-  font-size: 3vh;
-  line-height: 7vh;
-  color: black;
-  text-align: center;
-  background-color: white;
-  border: solid 0.5vh black;
-  border-radius: 0.5em;
-`
-
-const PlayerBlack = styled(PlayerWhite)`
-  margin: 9vh 0 24vh;
-  color: whitesmoke;
-  text-align: center;
-  background-color: black;
-  border: solid 0.5vh white;
-`
-
-const LogArea = styled.textarea`
-  width: 22vh;
-  height: 40vh;
-  margin-top: 9vh;
-  margin-bottom: 10vh;
-  font-size: 2vh;
-  resize: none;
-  border-radius: 4px;
-`
 const Modal = styled.div<{ isShow: boolean }>`
   position: absolute;
   top: 30%;
@@ -112,16 +69,9 @@ const Modal = styled.div<{ isShow: boolean }>`
   z-index: 2;
   display: ${({ isShow }) => (isShow ? 'None' : 'flex')};
   flex-direction: column;
+  align-items: center;
   background: whitesmoke;
   border-radius: 1.5em;
-
-  div {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    margin-top: 15%;
-  }
 `
 
 const ModalLable = styled.label`
@@ -139,30 +89,18 @@ const ModalBack = styled.div<{ isShow: boolean }>`
   left: 0;
   z-index: 1;
   display: ${({ isShow }) => (isShow ? '' : 'None')};
-  display: None;
+
+  /* display: None; */
   width: 100%;
   height: 100%;
   background-color: rgb(0 0 0 / 80%);
 `
-const InputBox = styled.input.attrs({ type: 'text', maxLength: 6 })`
-  width: 60%;
-  height: 40%;
-  font-size: 120%;
-  border: solid 0.1vh black;
-  border-radius: 0.2em;
 
-  :focus {
-    border: transparent 0.1vh solid;
-    outline: 0;
-    box-shadow: 0 0 0 0.2vh rgb(33 150 243) inset;
-  }
-`
-
-const InputButton = styled.button`
-  width: 15%;
-  height: 40%;
-  margin-left: 2%;
-  font-size: 120%;
+const ModalButton = styled.button`
+  width: 40%;
+  height: 30%;
+  margin-top: 10%;
+  font-size: 300%;
   background-color: #ddd;
   border: 0.1vh solid black;
   border-radius: 1.5em;
@@ -195,11 +133,16 @@ const AlertPrompt = styled.div`
     }
   }
 `
-const ButtonArea = styled(ColumnArea)`
+const ButtonArea = styled.div`
+  position: fixed;
+  right: 1vh;
+  bottom: 1vh;
+  display: flex;
+  flex-direction: column;
   height: 15vh;
   padding: 1vh;
   background-color: #dff;
-  border: solid 0.1vh;
+  border: solid 0.2vh black;
   border-radius: 1.5em;
 `
 
@@ -218,32 +161,33 @@ const EntryButton = styled.button`
     box-shadow: 0 0 0.1vh rgb(0 0 0 / 30%);
   }
 `
-const UserInfoArea = styled.div`
+const UserStateLabel = styled.div`
   position: fixed;
   bottom: 1vh;
   left: 1vh;
   display: flex;
   flex-direction: column;
-  padding: 0.7vh;
+  padding: 1vh;
+  font-size: 3.6vh;
   background-color: whitesmoke;
   border-left: solid 0.5vh gray;
   box-shadow: 0 3px 5px rgb(0 0 0 / 22%);
 `
 
-const UserInfoLabel = styled.label`
-  margin: 0.1vh 0;
-  font-size: 3vh;
-`
-
 const ButtonLabel = styled.label`
   margin: 0.5vh 0;
   font-size: 2vh;
+  text-align: center;
 `
 
 const Home: NextPage = () => {
   const boardInit = Array.from(new Array(8), () => new Array(8).fill(9))
-  const [board, setBoard] = useState(boardInit)
-  const [userInfo, setUserInfo] = useState({ userName: '匿名', userState: '観戦者' })
+  const userInfoInit = {
+    board: boardInit,
+    gameState: 'State',
+  }
+  const [gameInfo, setGameInfo] = useState(userInfoInit)
+  const [userState, setUserState] = useState('観戦者')
   const [isClickedStart, setIsClickedStart] = useState(false)
   const [isShowModal, setIsShowModal] = useState(true)
   const [isSocketCond, setIsSocketCond] = useState<null | boolean>(null)
@@ -254,13 +198,30 @@ const Home: NextPage = () => {
       reconnection: false,
     })
 
-    socket.current.on('board', (msg) => {
-      console.log(msg)
-      const b = msg.board
-      setBoard(b)
+    socket.current.on('board', (data) => {
+      console.log(data)
+      const b = data.board
+      setGameInfo({ ...gameInfo, board: b })
     })
-    socket.current.on('result', (msg) => {
-      console.log(msg)
+    socket.current.on('gameInfo', (data) => {
+      console.log(data)
+      const { board = gameInfo.board, gameState = gameInfo.gameState } = data
+      setGameInfo({
+        board: board,
+        gameState: gameState,
+      })
+    })
+    socket.current.on('btn_action_rep', (data) => {
+      const { type, isSuccses } = data
+      if (type === 'entry') {
+        if (isSuccses) {
+          window.alert('エントリーしました')
+          setUserState('対戦相手待機中')
+        }
+      }
+    })
+    socket.current.on('result', (data) => {
+      console.log(data)
     })
     socket.current.on('connect', () => {
       setIsSocketCond(true)
@@ -285,11 +246,20 @@ const Home: NextPage = () => {
   const registerUserInfo = () => {
     setIsClickedStart(true)
     setIsShowModal(false)
-    console.log('CL')
+    console.log(socket.current.id)
     /* socket.on('connect', () => {
       console.log(`socket.connectの中身：`)
       console.log(socket.id)
     })*/
+  }
+
+  const entry = () => {
+    if (window.confirm('エントリーしますか？')) {
+      alert('エントリーしました。')
+      socket.current.emit('entry')
+    } else {
+      alert('キャンセルされました。')
+    }
   }
 
   return (
@@ -301,63 +271,36 @@ const Home: NextPage = () => {
             {isSocketCond === null
               ? 'サーバーに接続中'
               : isSocketCond
-              ? 'ユーザー名を入力してください'
+              ? '接続完了!'
               : 'サーバーに接続できません'}
           </ModalLable>
-          {isSocketCond ? (
-            <div>
-              <InputBox
-                id="inputName"
-                value={userInfo.userName}
-                onChange={(e) => setUserInfo({ ...userInfo, userName: e.target.value })}
-              />
-              <InputButton id="inputName" type="submit" onClick={registerUserInfo}>
-                Start
-              </InputButton>
-            </div>
-          ) : (
-            ''
-          )}
+          <ModalButton id="inputName" type="submit" onClick={registerUserInfo}>
+            Start
+          </ModalButton>
         </Modal>
       </ModalBack>
-      <ColumnArea>
-        <PlayerBlack>黒 - 未着席さんだ</PlayerBlack>
-        <PlayerWhite>白 - 未着席さんだ</PlayerWhite>
-      </ColumnArea>
-      <ColumnArea>
-        <GameStateShow>参加者募集中</GameStateShow>
-        <Board>
-          {board.map((row, y) =>
-            row.map((num, x) => (
-              <Sqaure
-                key={`${x}-${y}`}
-                num={num}
-                onClick={() => {
-                  b(x, y)
-                }}
-              >
-                {num === 0 ? <WhiteDisk /> : num === 1 ? <BlackDisk /> : ''}
-              </Sqaure>
-            ))
-          )}
-        </Board>
-      </ColumnArea>
-      <ColumnArea>
-        <LogArea readOnly={true} />
-        <ButtonArea>
-          <ButtonLabel>参加はこちらから</ButtonLabel>
-          <EntryButton>Entry</EntryButton>
-        </ButtonArea>
-      </ColumnArea>
-      {isSocketCond ? (
-        <UserInfoArea>
-          {' '}
-          <UserInfoLabel>UserName : {userInfo.userName}</UserInfoLabel>
-          <UserInfoLabel>State : {userInfo.userState}</UserInfoLabel>
-        </UserInfoArea>
-      ) : (
-        ''
-      )}
+
+      <GameStateLabel> {gameInfo.gameState}</GameStateLabel>
+      <Board>
+        {gameInfo.board.map((row, y) =>
+          row.map((num, x) => (
+            <Sqaure
+              key={`${x}-${y}`}
+              num={num}
+              onClick={() => {
+                b(x, y)
+              }}
+            >
+              <Disk num={num} />
+            </Sqaure>
+          ))
+        )}
+      </Board>
+      <ButtonArea>
+        <ButtonLabel>参加はこちらから</ButtonLabel>
+        <EntryButton onClick={() => entry()}>Entry</EntryButton>
+      </ButtonArea>
+      {isSocketCond ? <UserStateLabel>State : {userState}</UserStateLabel> : ''}
     </Container>
   )
 }
