f29e4591ee0b1a377b99890531b3ed34a6da867a
s1f102100119 <s1f102100119@iniad.org>
2022-07-10 17:36:28 +0900

    ゲーム終了処理追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 
 const URL = 'http://localhost:8000'
 const COLOR = ['white', 'black']
-const USER_STATE = ['観戦者', '対戦相手待機中']
+const USER_STATE = ['観戦者', '対戦相手待機中', 'プレイヤー[白]', 'プレイヤー[黒]']
 
 const Container = styled.div`
   display: flex;
@@ -37,13 +37,7 @@ const Sqaure = styled.div<{ num: number }>`
   background-color: ${({ num }) => (num === 8 ? 'red' : '')};
   border: 0.2vh solid black;
 `
-
 const Disk = styled.div<{ num: number }>`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
   width: 5vh;
   height: 5vh;
   margin: auto;
@@ -51,14 +45,44 @@ const Disk = styled.div<{ num: number }>`
   border-radius: 50%;
 `
 
-const GameMsgLabel = styled.label`
-  height: 8vh;
-  margin: 0 0 1vh;
-  font-size: 3vh;
+const BoardDisk = styled(Disk)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 5vh;
+  height: 5vh;
+`
+
+const GameMsg = styled.div`
+  position: fixed;
+  top: 5vh;
+  width: 66vh;
+  padding: 0.2vh;
+  margin: 0 0 3vh;
+  font-size: 4vh;
   font-weight: bold;
   line-height: 7vh;
   color: black;
   text-align: center;
+  background: #eee;
+  border: 0.3vh solid #222;
+  border-radius: 0.5em;
+
+  span.white {
+    color: white;
+    text-shadow: 1px 1px 0 black, -1px -1px 0 black, -1px 1px 0 black, 1px -1px 0 black,
+      0 1px 0 black, 0 -1px 0 black, -1px 0 0 black, 1px 0 0 black;
+  }
+`
+const DiskCount = styled(GameMsg)`
+  top: initial;
+  bottom: 5vh;
+  display: flex;
+  width: initial;
+  padding: 0.8vh;
+  line-height: 4.5vh;
 `
 
 const Modal = styled.div<{ isShow: boolean }>`
@@ -90,8 +114,7 @@ const ModalBack = styled.div<{ isShow: boolean }>`
   left: 0;
   z-index: 1;
   display: ${({ isShow }) => (isShow ? '' : 'None')};
-
-  /* display: None; */
+  display: None;
   width: 100%;
   height: 100%;
   background-color: rgb(0 0 0 / 80%);
@@ -108,30 +131,6 @@ const ModalButton = styled.button`
 
   :hover {
     background-color: #ccc;
-  }
-`
-const AlertPrompt = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 5;
-  display: None;
-  justify-content: center;
-  width: 45%;
-  height: 10%;
-  margin-top: 2vh;
-  background-color: red;
-  border-radius: 1.5em;
-  animation: slide-in 1.6s;
-  @keyframes slide-in {
-    0% {
-      opacity: 0;
-      transform: translateY(-64px);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
   }
 `
 const ButtonArea = styled.div`
@@ -153,7 +152,7 @@ const EntryButton = styled.button`
   font-size: 4vh;
   background-color: #ddd;
 
-  /* c0c0c0 暗い #e6e6fa 明るめ */
+  /* #c0c0c0 暗い #e6e6fa 明るめ */
   border-bottom: solid 0.6vh #555;
   border-radius: 1.5em;
 
@@ -162,7 +161,7 @@ const EntryButton = styled.button`
     box-shadow: 0 0 0.1vh rgb(0 0 0 / 30%);
   }
 `
-const UserStateLabel = styled.div`
+const UserStateLabel = styled.div<{ userState: string }>`
   position: fixed;
   bottom: 1vh;
   left: 1vh;
@@ -170,7 +169,9 @@ const UserStateLabel = styled.div`
   flex-direction: column;
   padding: 1vh;
   font-size: 3.6vh;
-  background-color: whitesmoke;
+  color: ${({ userState }) => (userState === USER_STATE[3] ? 'white' : 'black')};
+  background-color: ${({ userState }) =>
+    userState === USER_STATE[2] ? 'white' : userState === USER_STATE[3] ? 'black' : 'whitesmoke'};
   border-left: solid 0.5vh gray;
   box-shadow: 0 3px 5px rgb(0 0 0 / 22%);
 `
@@ -183,36 +184,40 @@ const ButtonLabel = styled.label`
 
 const Home: NextPage = () => {
   const boardInit = Array.from(new Array(8), () => new Array(8).fill(9))
-  const userInfoInit = {
+  const gameInfoInit = {
     board: boardInit,
-    msg: 'State',
+    msg: 'INIT',
+    turnColor: 'black',
+    diskCount: { whiteCount: 0, blackCount: 0 },
+    isPlaying: false,
+    isGameOver: false,
   }
-  const [gameInfo, setGameInfo] = useState(userInfoInit)
+  const [gameInfo, setGameInfo] = useState(gameInfoInit)
   const [userState, setUserState] = useState(USER_STATE[0])
   const [isClickedStart, setIsClickedStart] = useState(false)
   const [isShowModal, setIsShowModal] = useState(true)
   const [isSocketCond, setIsSocketCond] = useState<null | boolean>(null)
   // eslint-disable-next-line
   const socket = useRef<Socket>(null!)
+  const zeroPadding = (num: number) => {
+    return `0${num}`.slice(-2)
+  }
+
   useEffect(() => {
     socket.current = io(URL, {
       reconnection: false,
     })
-
-    socket.current.on('board', (data) => {
-      console.log(data)
-      const b = data.board
-      setGameInfo({ ...gameInfo, board: b })
-    })
     socket.current.on('gameInfo', (data) => {
+      const { board = null, msg = null, diskCount = null, turnColor = null } = data
       console.log(data)
-      const { board = gameInfo.board, msg = gameInfo.msg } = data
-      setGameInfo({
-        board: board,
-        msg: msg,
-      })
+      if (board !== null) setGameInfo((current) => ({ ...current, board: board }))
+
+      if (msg !== null) setGameInfo((current) => ({ ...current, msg: msg }))
+
+      if (diskCount !== null) setGameInfo((current) => ({ ...current, diskCount: diskCount }))
+      if (turnColor !== null) setGameInfo((current) => ({ ...current, turnColor: turnColor }))
     })
-    socket.current.on('btn_action_rep', (data) => {
+    socket.current.on('btnActionRep', (data) => {
       const { type, isSuccses } = data
       if (type === 'entry') {
         if (isSuccses) {
@@ -221,8 +226,20 @@ const Home: NextPage = () => {
         }
       }
     })
-    socket.current.on('result', (data) => {
-      console.log(data)
+    socket.current.on('startReversi', (data) => {
+      const { color } = data
+      if (color === 'white') {
+        setUserState(USER_STATE[2])
+      } else if (color === 'black') {
+        setUserState(USER_STATE[3])
+      }
+      setGameInfo({ ...gameInfo, isPlaying: true })
+    })
+
+    socket.current.on('gameOver', (data) => {
+      const { result } = data
+      console.log(result)
+      setGameInfo((current) => ({ ...current, isGameover: true }))
     })
     socket.current.on('connect', () => {
       setIsSocketCond(true)
@@ -268,7 +285,6 @@ const Home: NextPage = () => {
 
   return (
     <Container>
-      <AlertPrompt />
       <ModalBack isShow={isShowModal}>
         <Modal isShow={isClickedStart}>
           <ModalLable>
@@ -288,7 +304,7 @@ const Home: NextPage = () => {
         </Modal>
       </ModalBack>
 
-      <GameMsgLabel> {gameInfo.msg}</GameMsgLabel>
+      <GameMsg> {gameInfo.msg}</GameMsg>
       <Board>
         {gameInfo.board.map((row, y) =>
           row.map((num, x) => (
@@ -299,16 +315,28 @@ const Home: NextPage = () => {
                 putDisk(x, y, num)
               }}
             >
-              <Disk num={num} />
+              <BoardDisk num={num} />
             </Sqaure>
           ))
         )}
       </Board>
-      <ButtonArea>
-        <ButtonLabel>参加はこちらから</ButtonLabel>
-        <EntryButton onClick={() => entry()}>Entry</EntryButton>
-      </ButtonArea>
-      {isSocketCond ? <UserStateLabel>State : {userState}</UserStateLabel> : ''}
+      <DiskCount>
+        <Disk num={0} />：{zeroPadding(gameInfo.diskCount.whiteCount)} -{' '}
+        {zeroPadding(gameInfo.diskCount.blackCount)}： <Disk num={1} />
+      </DiskCount>
+      {!gameInfo.isPlaying || userState === USER_STATE[2] || userState === USER_STATE[3] ? (
+        <ButtonArea>
+          <ButtonLabel>参加はこちらから</ButtonLabel>
+          <EntryButton onClick={() => entry()}>Entry</EntryButton>
+        </ButtonArea>
+      ) : (
+        ''
+      )}
+      {isSocketCond ? (
+        <UserStateLabel userState={userState}>State : {userState}</UserStateLabel>
+      ) : (
+        ''
+      )}
     </Container>
   )
 }
