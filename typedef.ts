a0ff60c843d28b85a35c9b8144791d6723dd4280
kado17 <kado17@users.noreply.github.com>
2022-07-17 07:42:15 +0000

    deploy: 350e96fed65802e03446013a292af4d92cbd2085

--- typedef.ts
+++ typedef.ts
@@ -4,27 +4,14 @@ export const Disc = {
   Putable: 8,
   Empty: 9,
 } as const
-
-export type GameState = 'playerWanted' | 'duringAGame' | 'gameResult'
 export type Disc = typeof Disc[keyof typeof Disc]
 export type PLColor = 'White' | 'Black' | 'NA'
+export type GameState = 'playerWanted' | 'duringAGame' | 'gameResult'
 export type UserState = 'spectator' | 'waiting' | 'PLWhite' | 'PLBlack'
 export type ViewandEmitKey = 'entry' | 'entryCancel' | 'gameCancel' | 'gameResultEnd' | ''
 
-export const colorConvJp: { [key in PLColor]: string } = {
-  White: '白',
-  Black: '黒',
-  NA: '引き分け',
-}
-export const colorConvDisc: { [key in PLColor]: Disc } = { White: 0, Black: 1, NA: 9 }
-
-export type NumberOfDisc = {
-  [key: string]: number
-  White: number
-  Black: number
-}
+export type GameInfoKey = 'board' | 'msg' | 'turnColor' | 'numberOfDisc' | 'gameState'
 export type GameInfoType = Disc[][] | string | PLColor | NumberOfDisc | boolean
-
 export type GameInfo = {
   [key: string]: Disc[][] | string | PLColor | NumberOfDisc | GameState
   board: Disc[][]
@@ -32,4 +19,10 @@ export type GameInfo = {
   turnColor: PLColor
   numberOfDisc: NumberOfDisc
   gameState: GameState
+}
+
+export type NumberOfDisc = {
+  [key: string]: number
+  White: number
+  Black: number
 }
